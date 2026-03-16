fe3e24c44827cc75d852a0aeac66d71e01b89f37
Dominik Matras <dominik.matras@nuvira.dev>
2026-03-16 20:20:34 +0100

    Finish day 7

--- day7/main.py
+++ day7/main.py
@@ -11,8 +11,8 @@ chosen_word = word_list[random.randint(0, len(word_list) - 1)]
 
 placeholder = ""
 
-for letter in chosen_word: 
-  placeholder += "_"
+for letter in chosen_word:
+    placeholder += "_"
 
 print(placeholder)
 
@@ -22,42 +22,39 @@ correct_letters = []
 lives = 6
 
 while not game_over:
-  print(f"***************** {lives}/6 LIVES *****************")
-  guess = input("Guess the letter: ").lower()
+    print(f"***************** {lives}/6 LIVES *****************")
+    guess = input("Guess the letter: ").lower()
 
-  display = ""
+    display = ""
 
-  if guess in correct_letters:
-    print("Hey! You already guessed this letter!")
+    if guess in correct_letters:
+        print("Hey! You already guessed this letter!")
 
-  for letter in chosen_word:
-    if letter == guess:
-      display += letter
-      correct_letters.append(guess)
-    elif letter in correct_letters:
-      display += letter
-    else: 
-      display += "_"
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
 
-  if guess not in chosen_word:
-    
-    lives -= 1
-    if lives == 0:
-      game_over = True
-      print(stages[lives])
-      print(f"\nIt was: {chosen_word.upper()}")
-      print("***************** YOU LOSE! *****************")
+    if guess not in chosen_word:
 
-  if "_" not in display:
-    game_over = True
-    print(f"\nThe word is: {chosen_word.upper()}")
-    print("****************** YOU WIN! *****************")
-    lives = 0
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print(stages[lives])
+            print(f"\nIt was: {chosen_word.upper()}")
+            print("***************** YOU LOSE! *****************")
 
-  if lives > 0:
-    print(stages[lives])
-    print("")
-    print(display)
+    if "_" not in display:
+        game_over = True
+        print(f"\nThe word is: {chosen_word.upper()}")
+        print("****************** YOU WIN! *****************")
+        lives = 0
 
-
-  
+    if lives > 0:
+        print(stages[lives])
+        print("")
+        print(display)
